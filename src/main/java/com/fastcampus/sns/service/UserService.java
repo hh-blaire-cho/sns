7ef40fcbf9a0321c0342700c7f7b0020c2e9b9cf
Blaire <hh.blaire.cho@gmail.com>
2024-09-29 20:53:02 +0900

    #2 impl simple login Service and Controller - TODO: auth tokenization

--- src/main/java/com/fastcampus/sns/service/UserService.java
+++ src/main/java/com/fastcampus/sns/service/UserService.java
@@ -25,4 +25,18 @@ public class UserService {
         return UserDto.fromEntity(savedUser); // NOTE: 반환할 때는 무조건 DTO!! (서비스는 DTO로 주고받아야함)
     }
 
+    // TODO JWT (로그인을 했을 때 암호화된 문자열 반환)
+    public UserDto login(String username, String password) {
+        // 회원가입 여부 확인
+        UserEntity userEntity = userRepository.findByUsername(username)
+            .orElseThrow(() -> new SnsAppException(ErrorCode.USER_NOT_FOUND, username));
+
+        // 비밀번호 확인
+        if (!userEntity.getPassword().equals(password)) {
+            throw new SnsAppException(ErrorCode.INVALID_PASSWORD);
+        }
+
+        //TODO 토큰을 생성하고 그 토큰을 반환해야지 DTO를 반한하면 보안 이슈임.
+        return UserDto.fromEntity(userEntity);
+    }
 }
