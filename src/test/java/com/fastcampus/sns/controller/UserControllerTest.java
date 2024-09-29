0c435ce3e38a0c7d8bafb2a5c1712eb4b7ef6041
Blaire <hh.blaire.cho@gmail.com>
2024-09-29 20:27:01 +0900

    #1 User Controller (Register) and its test

---
+++ src/test/java/com/fastcampus/sns/controller/UserControllerTest.java
@@ -0,0 +1,75 @@
+package com.fastcampus.sns.controller;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fastcampus.sns.dto.UserDto;
+import com.fastcampus.sns.dto.request.UserRegisterRequest;
+import com.fastcampus.sns.exception.ErrorCode;
+import com.fastcampus.sns.exception.GlobalControllerAdvice;
+import com.fastcampus.sns.exception.SnsAppException;
+import com.fastcampus.sns.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Import(GlobalControllerAdvice.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    UserService userService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private final String apiHeader = "/api/v1/users";
+
+
+    @Test
+    @DisplayName("회원가입 정상케이스")
+    public void test_userRegister() throws Exception {
+        String username = "id";
+        String password = "pw";
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest(username, password);
+        when(userService.register(username, password)).thenReturn(mock(UserDto.class));
+
+        mockMvc.perform(post(apiHeader + "/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(userRegisterRequest))
+            )
+            .andDo(print())
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("회원가입을 이미 있는 아이디로 시도하면 에러")
+    public void test_userRegister_withDuplicateName() throws Exception {
+        String username = "id";
+        String password = "pw";
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest(username, password);
+        when(userService.register(username, password)).thenThrow(new SnsAppException(ErrorCode.DUPLICATED_USER_NAME));
+
+        mockMvc.perform(post(apiHeader + "/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(userRegisterRequest))
+            )
+            .andDo(print())
+            .andExpect(status().is(ErrorCode.DUPLICATED_USER_NAME.getStatus().value()));
+    }
+
+}
